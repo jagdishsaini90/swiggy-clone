13cafe40e57e0f4e8d052db273d7f2f38d0e44c0
jagdishsaini90 <jsaini8941@gmail.com>
2023-09-22 20:02:14 +0530

    updated changes

--- src/appwrite.js
+++ src/appwrite.js
@@ -4,7 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("6501ddeac072abeb433e");
+  .setProject("650da519e3edf8f81b11");
 
 export const APPWRITE_ACCOUNT = new Account(client);
 export const APPWRITE_ID = ID;
